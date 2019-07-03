ed764cfd476ff96255895752e62feb6274056f48
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-02 20:56:18 -0700

    PKs placw3

---
+++ Numberlists.py
@@ -0,0 +1,9 @@
+# Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list.
+
+multiples_of_three = [3,6,9,12,15,18,21,24,27,30]
+for multiple in multiples_of_three:
+    print (multiple * multiple * multiple)
+
+print (multiples_of_three)
+
+
