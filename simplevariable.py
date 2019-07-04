ba3a96a2daa9ff5208d6a3eeb6cf6ddc7d17126f
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-03 17:09:45 -0700

    homework question

---
+++ simplevariable.py
@@ -0,0 +1,20 @@
+# Homework 2-1 ---> 2-2
+
+message = "Hello ms. jasmine!"
+print(message)
+
+message = "Welcome to Python crash course reader!"
+print(message)
+
+message = ("Hello eric, would you like learn some Python today?")
+print (message)
+print( message.title())
+
+first_name = "albert"
+last_name  = "einstein"
+full_name = first_name + " " + last_name
+print (full_name)
+print( "A person who never made a mistake never tried anything new."  + full_name.title() + ".")
+
+famous_person = ('steve jobs')
+print ("There is no reason to follow your heart." + famous_person.title() + ".")
