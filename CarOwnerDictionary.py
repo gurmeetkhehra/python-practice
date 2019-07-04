294684b44ca812e48df7f637a7666ec6c02d5d5e
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-04 11:03:16 -0700

    CityZipcode, CarOwnerDictionary

--- CarOwnerDictionary.py
+++ CarOwnerDictionary.py
@@ -2,5 +2,6 @@ car_owners = { 'Connie': 'Audi', 'Mandip': 'Volvo', 'Jasmine': 'Toyota', 'Kathy'
 
 #print (car_owners.keys())
 
-for owner_name in car_owners.keys():
+for owner_name, car_name in car_owners.items():
     print('Owner name:' + owner_name)
+    print('car name: ' + car_name)
---
+++ CityZipcode.py
@@ -0,0 +1,9 @@
+city_zipcode = {
+    'Milpitas': 95035,
+    'Evergreen': 95148,
+    'San Jose': 95112,
+    'Santa Clara': 95152
+}
+for k, v in city_zipcode.items():
+    print('city:' + k)
+    print('zipcode:' + str(v))
