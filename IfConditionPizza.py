f894d2c67b969be22783bf3b9bc19bc8d074cbf6
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-04 17:11:37 -0700

    second modfied

--- IfConditionPizza.py
+++ IfConditionPizza.py
@@ -1,25 +1,40 @@
-pizza_topping = ['Olive', 'Mushroom', 'Tomato', 'Cheese', 'Pineapple']
-if 'Olive' in pizza_topping:
-    print('Adding Olive')
-if 'Mushroom' in pizza_topping:
-    print('Adding Mushroom')
-if 'Tomato' in pizza_topping:
-    print('Adding Tomato')
-if 'Cheese' in pizza_topping:
-    print('Adding Cheese')
-if 'Pineapple' in pizza_topping:
-    print('Adding Pineapple')
-if 'Garlic' in pizza_topping:
-    print('Adding Garlic')
-if 'Olive' in pizza_topping:
-    print('Adding Olive')
-if 'Mushroom' in pizza_topping:
-    print('Adding Mushroom')
-if 'Tomato' in pizza_topping:
-    print('Adding Tomato')
-if 'Cheese' in pizza_topping:
-    print('Adding Cheese')
-if 'Pineapple' in pizza_topping:
-    print('Adding Pineapple')
+# pizza_topping = ['Olive', 'Mushroom', 'Tomato', 'Cheese', 'Pineapple']
+# if 'Olive' in pizza_topping:
+#     print('Adding Olive')
+# if 'Mushroom' in pizza_topping:
+#     print('Adding Mushroom')
+# if 'Tomato' in pizza_topping:
+#     print('Adding Tomato')
+# if 'Cheese' in pizza_topping:
+#     print('Adding Cheese')
+# if 'Pineapple' in pizza_topping:
+#     print('Adding Pineapple')
+# if 'Garlic' in pizza_topping:
+#     print('Adding Garlic')
+# if 'Olive' in pizza_topping:
+#     print('Adding Olive')
+# if 'Mushroom' in pizza_topping:
+#     print('Adding Mushroom')
+# if 'Tomato' in pizza_topping:
+#     print('Adding Tomato')
+# if 'Cheese' in pizza_topping:
+#     print('Adding Cheese')
+# if 'Pineapple' in pizza_topping:
+#     print('Adding Pineapple')
 
+pizza_topping = ['Pepperoni', 'Chicken', 'Pork', 'Cheese', 'Beef']
+if 'Pepperoni' in pizza_topping:
+    print ('Put on the Pepperoni ')
+
+if 'Chicken' in pizza_topping:
+    print ('Put on the Chicken')
+
+if 'Pork' in pizza_topping:
+    print ('Put on the Pork')
+
+if 'Cheese' in pizza_topping:
+    print ('Put on the Cheese')
+
+if 'Beef' in pizza_topping:
+    print ('Put on the Beef')
 
