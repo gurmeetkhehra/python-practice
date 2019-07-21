5007de481c5dca95ce5119d346ff42f6cbee1773
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 08:58:31 -0700

    evenodd

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+input_number = input ("Give me number if even or odd")
+converted_input = int (input_number)
+
+print(converted_input)
+
+if converted_input % 2 == 0:
+    print("even number")
+
+else:
+    print ("odd number")
