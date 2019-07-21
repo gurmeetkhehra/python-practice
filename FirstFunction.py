129f77b9abeee93fe3248702235c1384fd17db2c
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 10:10:07 -0700

    firstfunction

--- FirstFunction.py
+++ FirstFunction.py
@@ -1,10 +1,9 @@
-def greet_user():
-    print('Hello user')
-    print ('Hello from Jasmine')
+def greet_user(username, age):
+    print('Hello user ' + username.title() + ' with age ' + str(age))
 
 counter = 1
 while counter < 5:
-    greet_user()
+    greet_user('Khivi', 4)
     counter = counter + 1
 
 
