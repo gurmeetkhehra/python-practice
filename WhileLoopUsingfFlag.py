6a39698578a12d5325970ed22921a5a69fc8d592
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 09:28:00 -0700

    whilemessageinput

---
+++ WhileLoopUsingfFlag.py
@@ -0,0 +1,10 @@
+active = True
+while active:
+    message = input ("Enter your message. Enter quit the loop.")
+    message = message.strip()
+    if message == 'quit':
+        active = False
+    else:
+        print(active)
+
+# 'quit' == ' quit'
