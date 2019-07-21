4c898f0bd8ace0c017069dbae86d1e5e6e3907be
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 11:14:30 -0700

    j

--- MakePizza.py
+++ MakePizza.py
@@ -1,5 +1,7 @@
-def make_pizza(*toppings):
-    print(toppings)
+def make_pizza(type, size, *toppings):
+    print(type, size, toppings)
 
-make_pizza('tomatoes', 'mushroom')
-make_pizza('pineapple', 'olive', 'Jalepanos')
+make_pizza('flat bread', 'X-SMALL','tomatoes', 'mushroom')
+make_pizza('Deep Dish', 'Large','pineapple', 'olive', 'Jalepanos')
+
+make_pizza ('deepdish', 'small','pineapple', 'olive', 'Jalepanos')
