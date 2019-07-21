c2c85617692d220c46c8da8e309d7eddfc5724ec
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 08:44:28 -0700

    userinput

---
+++ userinput.py
@@ -0,0 +1,9 @@
+input_number = input ("Give me a numbers for 401K contribution")
+
+# input_number=float(input_number)
+# input_number = input_number +(input_number * .5)
+# print (input_number)
+
+input_number = int (input_number)
+input_number = input_number * 2
+print (input_number)
