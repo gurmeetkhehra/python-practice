971f20f9d128b7528d7116e9f314de1e478a362c
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 09:46:07 -0700

    whilelooplist

---
+++ WhileLoopList.py
@@ -0,0 +1,11 @@
+# cars = ['Audi','Toyota', 'Nissan','Audi','Audi','Audi','Audi','Audi','Audi']
+# while 'Audi' in cars:
+#     cars.remove('Audi')
+#
+# print (cars)
+
+cities = ['San Jose', 'Fremont', 'Oakland', 'San Francisco', 'Riverside']
+while 'Oakland' in cities:
+    cities.remove('Oakland')
+
+print(cities)
