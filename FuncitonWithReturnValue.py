2013f8c54317b23fc9fbf9533d836515cd2f7390
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 10:46:46 -0700

    functionwithreturnvalue

---
+++ FuncitonWithReturnValue.py
@@ -0,0 +1,5 @@
+def get_formatted_name (first_name, last_name):
+    full_name = first_name.upper() + ' ' + last_name.upper();
+    return full_name;
+
+print(get_formatted_name('Jasmine', 'Khehra'))
