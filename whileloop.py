2b1558c7791cf59bb50df08192f4921352a18f36
jasminekhehra <khehragurmeet5@gmail.com>
2019-07-21 09:08:02 -0700

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,9 @@
+counter = 1
+while counter < 50:
+    print("I like Python")
+    counter = counter + 10
+
+    counter = 1
+    while counter < 10:
+        print("I like Python")
+        counter = counter + 1
